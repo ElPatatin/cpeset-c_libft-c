6b8cae484ffbe43d6742eb9cc1c4f544627cb1df
ElPatatin <cristobalpes1@gmail.com>
2022-10-07 09:48:11 +0200

    deupdated the ft_printf lib

--- library/ft_printf/src/ft_printf.c
+++ library/ft_printf/src/ft_printf.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cpeset-c <cpeset-c@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cpeset-c <cpeset-c@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 14:32:26 by cpeset-c          #+#    #+#             */
-/*   Updated: 2022/08/27 18:22:46 by cpeset-c         ###   ########.fr       */
+/*   Updated: 2022/10/07 09:47:42 by cpeset-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ft_printf.h"
 
 static t_vars	ft_init_vars(const char *fmt);
+static int		ft_print_check(t_vars *vars);
 
 int	ft_printf(const char *fmt, ...)
 {
@@ -39,6 +40,28 @@ int	ft_printf(const char *fmt, ...)
 	}
 	va_end(vars.args);
 	return (vars.bytes);
+}
+
+static int
+	ft_print_check(t_vars *vars)
+{
+	if (vars->c == '%')
+		return (ft_char('%'));
+	else if (vars->c == 'c')
+		return (ft_char(va_arg(vars->args, int)));
+	else if (vars->c == 's')
+		return (ft_string(va_arg(vars->args, char *)));
+	else if (vars->c == 'd' || vars->c == 'i')
+		return (ft_nbrbase(va_arg(vars->args, int), 10, vars->c));
+	else if (vars->c == 'u')
+		return (ft_nbrbase(va_arg(vars->args, int), 10, vars->c));
+	else if (vars->c == 'p')
+		return (ft_vptr(va_arg(vars->args, t_unll)));
+	else if (vars->c == 'x' || vars->c == 'X')
+		return (ft_nbrbase(va_arg(vars->args, unsigned int), 16, vars->c));
+	else
+		return (0);
+	return (1);
 }
 
 static t_vars	ft_init_vars(const char *fmt)
